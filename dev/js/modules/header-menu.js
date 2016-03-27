a600e5b9cf64b468c2d209132a277cc6971766d4
Sergey Rubets <srubets@speroteck.com>
2016-03-27 14:10:43 +0300

    Bugfixing.

--- dev/js/modules/header-menu.js
+++ dev/js/modules/header-menu.js
@@ -8,19 +8,8 @@
   };
 
   function attachEvent() {
-    $('.header_list').on('click', '.header_item', setActive);
     $('.hamburger').on('click', toggleMenu);
   }
-
-  function setActive(e) {
-    e.preventDefault();
-
-    $(this)
-      .addClass('header_item--active')
-      .siblings()
-      .removeClass('header_item--active')
-  }
-
 
   function toggleMenu() {
     var target = $('.header_nav');
